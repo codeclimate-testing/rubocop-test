494e7e0bd3c81a38f79fad3bbe5752d38ce1256d
Federico Moyá <federicomoyamartin@gmail.com>
2021-01-05 16:09:06 -0300

    add rubocop

--- foo.rb
+++ foo.rb
@@ -1,7 +1,24 @@
+# frozen_string_literal: true
+
+# This is the Foo class
 class Foo
   attr_reader :bar
 
-  def initialize(bar)
+  def initialize(_bar)
     @bar = @bar
+  end
+
+  def send_messge
+    message(
+      'Some',
+      'Random',
+      'Message'
+    )
+  end
+
+  private
+
+  def message(text_a, text_b, text_c)
+    puts "#{text_a} - #{text_b} - #{text_c}"
   end
 end
